676fe5fba2c3b2679ae495c8dc3368dbdb2f3d41
madanamoninaresh <madanamoninaresh@gmail.com>
2025-12-22 18:28:43 +0530

    all changes are committed

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,7 +19,7 @@ module.exports.registerUser = async (req, res) => {
                     });
                     let token = generateToken(user)
                     res.cookie("token", token)
-                    res.send("user created succesfully");
+                    res.send("user registered successfully!please login.");
                 };
             });
         })
